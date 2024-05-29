1273a90cc76157e4913c6f7fdeb411963cb3fea0
Manfreedhm <162812669+Manfreedhm@users.noreply.github.com>
2024-05-28 23:05:35 -0600

    Add files via upload

---
+++ proyectoCompi/src/codigo/Tokens.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+/**
+ *
+ * @author efgarcid
+ */
+public enum Tokens {
+    linea,
+    Identificador,
+    BYTE,
+    SHORT,
+    INT,
+    LONG,
+    FLOAT,
+    DOUBLE,
+    BOOLEAN,
+    CHAR,
+    VERDADERO,
+    FALSO,
+    STRING,
+    puntoComa,
+    Numero,
+    coma,
+    igual,
+    asterisco,
+    ERROR,
+    RETURN,
+    include,
+    llaveApertura,
+    llaveCierre,
+    signoMenorQue,
+    signoMayorQue,
+    signoDosPuntos,
+    signoAlmohoadilla,
+    signoComa,
+    signoMas,
+    signoMenos,
+    signoMultiplicacion,
+    signoSlash,
+    parentesisApertura,
+    parentesisCierre,
+    signoPunto,
+    signoComilla,
+    signoOr,
+    using,
+    namespace,
+    classT,
+    publicT,
+    doubleT,
+    charT,
+    switchT,
+    caseT,
+    returnT,
+    defaultT,
+    endlT,
+    pragmaT,
+    onceT,
+    whileT,
+    continueT,
+    ifT,
+    elseT,
+    trueT,
+    falseT,
+    DAY
+}
